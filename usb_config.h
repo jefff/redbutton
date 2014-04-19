e89b01681b9925212572d83db4944f25c41b8dc5
jefff <gmjefff@gmail.com>
2014-04-19 02:09:00 -0700

    Removed code for other microcontrollers.

--- usb_config.h
+++ usb_config.h
@@ -1,24 +1,3 @@
-/*
- * USB HID Mouse Configuration
- *
- * This file may be used by anyone for any purpose and may be used as a
- * starting point making your own application using M-Stack.
- *
- * It is worth noting that M-Stack itself is not under the same license
- * as this file.
- *
- * M-Stack is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  For details, see sections 7, 8, and 9
- * of the Apache License, version 2.0 which apply to this file.  If you have
- * purchased a commercial license for this software from Signal 11 Software,
- * your commerical license superceeds the information in this header.
- *
- * Alan Ott
- * Signal 11 Software
- * 2013-04-08
- */
-
 #ifndef USB_CONFIG_H__
 #define USB_CONFIG_H__
 
@@ -44,12 +23,9 @@
 	PPB_ALL          - Ping-pong all endpoints
 	PPB_EPN_ONLY     - Ping-pong all endpoints except 0
 */
-#ifdef __PIC32MX__
-	/* PIC32MX only supports PPB_ALL */
-	#define PPB_MODE PPB_ALL
-#else
-	#define PPB_MODE PPB_NONE
-#endif
+
+#define PPB_MODE PPB_ALL
+
 
 /* Comment the following line to use polling USB operation. When using polling,
    You are responsible for calling usb_service() periodically from your
--- usb_descriptors.c
+++ usb_descriptors.c
@@ -1,33 +1,9 @@
-/*
- * USB Descriptors file
- *
- * This file may be used by anyone for any purpose and may be used as a
- * starting point making your own application using M-Stack.
- *
- * It is worth noting that M-Stack itself is not under the same license as
- * this file.
- *
- * M-Stack is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  For details, see sections 7, 8, and 9
- * of the Apache License, version 2.0 which apply to this file.  If you have
- * purchased a commercial license for this software from Signal 11 Software,
- * your commerical license superceeds the information in this header.
- *
- * Alan Ott
- * Signal 11 Software
- */
-
 #include "usb_config.h"
 #include "usb/usb.h"
 #include "usb/usb_ch9.h"
 #include "usb/usb_hid.h"
 
-#ifdef __C18
-#define ROMPTR rom
-#else
 #define ROMPTR
-#endif
 
 /* Configuration Packet
  *
